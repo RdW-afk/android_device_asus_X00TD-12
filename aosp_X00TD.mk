e8b87134d6830999c39c3ef4b66a713dfa04109a
mrzero <rbesar210311@gmail.com>
2022-07-27 15:36:55 +0000

    Arcana BringUp!!

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -9,14 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common arcana stuff
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+ARCANA_MAINTAINER := Mr_ZerO
+TARGET_SUPPORTS_BLUR := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := false
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := aosp_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
