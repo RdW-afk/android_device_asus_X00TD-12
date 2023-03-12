39c567751ac9e8143afc4609d6b27b719077e319
Mr_ZerO <71553416+RdW-afk@users.noreply.github.com>
2023-03-12 17:18:05 +0700

    update

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -10,18 +10,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common BananaDroid stuff
-$(call inherit-product, vendor/banana/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-#Official Stuff
-BANANA_BUILD_TYPE := OFFICIAL
-BANANA_MAINTAINER := Romeo_Delta_Whiskey
-TARGET_FACE_UNLOCK_SUPPORTED := true
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := banana_X00TD
+PRODUCT_NAME := aosp_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -32,7 +30,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ1A.230105.002 9325679 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ1A.230205.002 9471150 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ1A.230105.002/9325679:user/release-keys"
+BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ1A.230205.002/9471150:user/release-keys"
